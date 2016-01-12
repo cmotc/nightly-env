25f662fb53a8e0b3290b57492ac8509fe1ad0625
cmotc <cmotc@openmailbox.org>
2016-01-11 22:21:44 -0500

    suppressed not found errors from clean, returned to apt-git version of genrepo which is going to get a better update next

--- helper.sh
+++ helper.sh
@@ -4,9 +4,9 @@ USE_DEBSH_SCRIPTS="Y"
 USE_RPMSH_SCRIPTS="N"
 USE_DROIDSH_SCRIPTS="N"
 if [ "$1" = "no-scripts" ]; then
-	USE_DEBSH_SCRIPTS="N"
-	USE_RPMSH_SCRIPTS="N"
-	USE_DROIDSH_SCRIPTS="N"
+    USE_DEBSH_SCRIPTS="N"
+    USE_RPMSH_SCRIPTS="N"
+    USE_DROIDSH_SCRIPTS="N"
 fi
 
 export WORKDIR=$(pwd)
@@ -15,249 +15,250 @@ echo "Loading build helper scripts in $WORKDIR"
 
 #clean up everything but unfinished folders
 clean(){
-	cd $WORKDIR
-	rm *.build *.dsc *.deb  *.changes *.tar.gz *.*.tar.gz *.tar.xz *.*.tar.xz *.tar.bz2 *.*.tar.bz2 *.apk
-	rm log
+    cd $WORKDIR
+    rm *.build *.dsc *.deb  *.changes *.tar.gz *.*.tar.gz *.tar.xz *.*.tar.xz *.tar.bz2 *.*.tar.bz2 *.apk &> /dev/null
+    rm log
 }
 
 #clean up everything including unfinished folders
 clobber(){
-	cd $WORKDIR
-	clean
-	dustup
+    cd $WORKDIR
+    clean
+    dustup
 }
 
 #clean up everything but the built packages
 dustup(){
-	cd $WORKDIR
-	for d in *; do
-		day=$(date +%Y%m%d)
-		until [ $(echo "$day" | grep "00") ]; do
-			rm -rf "$d-$day" 2> /dev/null
-			day=$((day-1))
-		done
-	done
+    cd $WORKDIR
+    for d in *; do
+        day=$(date +%Y%m%d)
+        until [ $(echo "$day" | grep "00") ]; do
+            rm -rf "$d-$day" 2> /dev/null
+            day=$((day-1))
+        done
+    done
 }
 
 #Helper for building debian packages with no alterations
 deb_nosh(){
-	d="$1"
-	echo "$d/debian.sh file not found. Attempting to build package automatically."
-	DEBFOLDERNAME="$d""_"$(date +%Y%m%d)
-	cd "$d" && git pull && cd ..
-	cp -Rv "$d" $DEBFOLDERNAME
-	tar -czvf $DEBFOLDERNAME.orig.tar.gz $DEBFOLDERNAME
-	t="false"
-	cd $DEBFOLDERNAME && t="true" && debuild -us -uc >> ../log
-	cd $WORKDIR
+    d="$1"
+    echo "$d/debian.sh file not found. Attempting to build package automatically."
+    DEBFOLDERNAME="$d""_"$(date +%Y%m%d)
+    cd "$d" && git pull && cd ..
+    cp -Rv "$d" $DEBFOLDERNAME
+    tar -czvf $DEBFOLDERNAME.orig.tar.gz $DEBFOLDERNAME
+    t="false"
+    cd $DEBFOLDERNAME && t="true" && debuild -us -uc >> ../log
+    cd $WORKDIR
 }
 #Helper for building redhat packages with no alterations
 rpm_nosh(){
-	d="$1"
-	echo "I don't know how to build rpm packages yet so $d.rpm does not build. Skipping and proceeding normally."
-	cd $WORKDIR
+    d="$1"
+    echo "I don't know how to build rpm packages yet so $d.rpm does not build. Skipping and proceeding normally."
+    cd $WORKDIR
 }
 
 #Helper for building android packages with no alterations
 droid_nosh(){
-	d="$1"
-	$DROIDFOLDERNAME="$d""_"$(date +%Y%m%d)
-	cd "$d" && git pull && cd ../
-	cd "$d" && git pull && cd ..
-	cp -Rv "$d" $DROIDFOLDERNAME
+    d="$1"
+    $DROIDFOLDERNAME="$d""_"$(date +%Y%m%d)
+    cd "$d" && git pull && cd ../
+    cd "$d" && git pull && cd ..
+    cp -Rv "$d" $DROIDFOLDERNAME
 #You know what? Fuck it. Might be useful.
-	tar -czvf $DROIDFOLDERNAME.orig.tar.gz $DROIDFOLDERNAME
-	t="false"
-	cd $DROIDFOLDERNAME && ant build && t="true" >> ../log
-	if [ ! $t = "true" ]; then
-		ant debug
-		t="true"
-	fi
-	if [ $t = "true" ]; then
-		cd $WORKDIR
-		cp $DROIDFOLDERNAME/bin/*.apk ./
-	fi
-	cd $WORKDIR
+    tar -czvf $DROIDFOLDERNAME.orig.tar.gz $DROIDFOLDERNAME
+    t="false"
+    cd $DROIDFOLDERNAME && ant build && t="true" >> ../log
+    if [ ! $t = "true" ]; then
+        ant debug
+        t="true"
+    fi
+    if [ $t = "true" ]; then
+        cd $WORKDIR
+        cp $DROIDFOLDERNAME/bin/*.apk ./
+    fi
+    cd $WORKDIR
 }
 
 #build all packages in all subdirectories
 build(){
-	cd $WORKDIR
-	clean
-	alias dh_make="dh_make --yes"
-	echo "$1"
-	if [ "$1" != "" ]; then
-		echo "$1"
-		if [ "$1" != "upload" ]; then
-			d="$1"
-			echo "$d"
-			if [ -f "$d/debian.sh" ]; then
-				if [ "$USE_DEBSH_SCRIPTS" = "Y" ]; then
-					. "$d/debian.sh" && echo "<<<Built $DEBFOLDERNAME>>>" && rm -rf $DEBFOLDERNAME 
-					cd $WORKDIR
-				fi
-			else
-				if [ -d "$d/debian" ]; then
-					deb_nosh "$d"
-				fi
-			fi
-			if [ -f "$d/rpm.sh" ]; then
-				if [ "$USE_RPMSH_SCRIPTS" = "y" ]; then
-					. "$d/rpm.sh" && echo "<<<Built $RPMFOLDERNAME>>>" && rm -rf $RPMFOLDERNAME #&& cd ..
-					cd $WORKDIR
-				fi
-			else
-				if [ -f "$d/*.spec"]; then
-					rpm_nosh "$d"
-				fi
-			fi
-			if [ -f "$d/droid.sh" ]; then
-				if [ "$USE_DROIDSH_SCRIPTS" = "y" ]; then
-					. "$d/rpm.sh" && echo "<<<Built $DROIDFOLDERNAME>>>" && rm -rf $DROIDFOLDERNAME #&& cd ..
-					cd $WORKDIR
-				fi
-			else
-				droid_nosh "$d"
-			fi
-		fi
-	else
-		for d in *; do
-			if [ -f "$d/debian.sh" ]; then
-				if [ "$USE_DEBSH_SCRIPTS" = "Y" ]; then
-					. "$d/debian.sh" && echo "<<<Built $DEBFOLDERNAME>>>" && rm -rf $DEBFOLDERNAME 
-					cd $WORKDIR
-				fi
-			else
-				if [ -d "$d/debian" ]; then
-					deb_nosh "$d"
-				fi
-			fi
-			if [ -f "$d/rpm.sh" ]; then
-				if [ "$USE_RPMSH_SCRIPTS" = "y" ]; then
-					. "$d/rpm.sh" && echo "<<<Built $RPMFOLDERNAME>>>" && rm -rf $RPMFOLDERNAME #&& cd ..
-					cd $WORKDIR
-				fi
-			else
-				if [ -f "$d/*.spec"]; then
-					rpm_nosh "$d"
-				else
-					echo "Not enough information to build .rpm package"
-				fi
-			fi
-			if [ -f "$d/droid.sh" ]; then
-				if [ "$USE_DROIDSH_SCRIPTS" = "y" ]; then
-					. "$d/rpm.sh" && echo "<<<Built $DROIDFOLDERNAME>>>" && rm -rf $DROIDFOLDERNAME #&& cd ..
-					cd $WORKDIR
-				fi
-			else
-				droid_nosh "$d"
-			fi
-		done
-	fi
-	if [ "$1" = "upload" ] ; then
-		upload
-	fi
+    cd $WORKDIR
+    clean
+    alias dh_make="dh_make --yes"
+    echo "$1"
+    if [ "$1" != "" ]; then
+        echo "$1"
+        if [ "$1" != "upload" ]; then
+            d="$1"
+            echo "$d"
+            if [ -f "$d/debian.sh" ]; then
+                if [ "$USE_DEBSH_SCRIPTS" = "Y" ]; then
+                    . "$d/debian.sh" && echo "<<<Built $DEBFOLDERNAME>>>" && rm -rf $DEBFOLDERNAME 
+                    cd $WORKDIR
+                fi
+            else
+                if [ -d "$d/debian" ]; then
+                    deb_nosh "$d"
+                fi
+            fi
+            if [ -f "$d/rpm.sh" ]; then
+                if [ "$USE_RPMSH_SCRIPTS" = "y" ]; then
+                    . "$d/rpm.sh" && echo "<<<Built $RPMFOLDERNAME>>>" && rm -rf $RPMFOLDERNAME #&& cd ..
+                    cd $WORKDIR
+                fi
+            else
+                if [ -f "$d/*.spec"]; then
+                    rpm_nosh "$d"
+                fi
+            fi
+            if [ -f "$d/droid.sh" ]; then
+                if [ "$USE_DROIDSH_SCRIPTS" = "y" ]; then
+                    . "$d/rpm.sh" && echo "<<<Built $DROIDFOLDERNAME>>>" && rm -rf $DROIDFOLDERNAME #&& cd ..
+                    cd $WORKDIR
+                fi
+            else
+                droid_nosh "$d"
+            fi
+        fi
+    else
+        for d in *; do
+            if [ -f "$d/debian.sh" ]; then
+                if [ "$USE_DEBSH_SCRIPTS" = "Y" ]; then
+                    . "$d/debian.sh" && echo "<<<Built $DEBFOLDERNAME>>>" && rm -rf $DEBFOLDERNAME 
+                    cd $WORKDIR
+                fi
+            else
+                if [ -d "$d/debian" ]; then
+                    deb_nosh "$d"
+                fi
+            fi
+            if [ -f "$d/rpm.sh" ]; then
+                if [ "$USE_RPMSH_SCRIPTS" = "y" ]; then
+                    . "$d/rpm.sh" && echo "<<<Built $RPMFOLDERNAME>>>" && rm -rf $RPMFOLDERNAME #&& cd ..
+                    cd $WORKDIR
+                fi
+            else
+                if [ -f "$d/*.spec"]; then
+                    rpm_nosh "$d"
+                else
+                    echo "Not enough information to build .rpm package"
+                fi
+            fi
+            if [ -f "$d/droid.sh" ]; then
+                if [ "$USE_DROIDSH_SCRIPTS" = "y" ]; then
+                    . "$d/rpm.sh" && echo "<<<Built $DROIDFOLDERNAME>>>" && rm -rf $DROIDFOLDERNAME #&& cd ..
+                    cd $WORKDIR
+                fi
+            else
+                droid_nosh "$d"
+            fi
+        done
+    fi
+    if [ "$1" = "upload" ] ; then
+        upload
+    fi
+    dustup
 }
 
 #Pull down all the updates from the source repositories.
 update(){
-        cd $WORKDIR
-	for d in *; do
-		if [ -d "$d/.git" ]; then
-			cd $d
-			git pull origin master
-			cd $WORKDIR
-		fi
-        done
+    cd $WORKDIR
+    for d in *; do
+        if [ -d "$d/.git" ]; then
+            cd $d
+            git pull origin master
+            cd $WORKDIR
+        fi
+    done
 }
 
 #Sign and update packages in remote repository
 upload(){
-	cd $WORKDIR
-	rm -rf ../repository/packages/
-	mkdir -p ../repository/packages/
-	cp *.build *.changes *.tar.xz *.dsc *.deb ../repository/packages/
-	if [ ! -d ../repository ]; then
-		genrepo
-	fi
-	cd $WORKDIR
-	cd ../repository/
-	rm -rf debian
-	./apt-git --check
+    cd $WORKDIR
+    rm -rf ../repository/packages/
+    mkdir -p ../repository/packages/
+    cp *.build *.changes *.tar.xz *.dsc *.deb ../repository/packages/
+    if [ ! -d ../repository ]; then
+        genrepo
+    fi
+    cd $WORKDIR
+    cd ../repository/
+    rm -rf debian
+    ./apt-git --check
 }
 
 #generate regeneration script
 genclone(){
-	cd $WORKDIR
-	GLOBIGNORE=.
-	echo "#! /bin/sh" > ./.clone
-	for subdir in *; do
-		test -d "$subdir" || continue
-		cd "$subdir"
-		remote=$(git remote -v)
-		for word in $remote; do
-			if [ `echo "$word" | grep 'git@' ` ]; then
-				echo "git clone $word" >> ../clone-$(date +%Y%m%d)
-			fi
-		done
-		cd -
-	done
-	awk '!seen[$0]++' ./clone-$(date +%Y%m%d) > ./.clone
-	rm ./clone-$(date +%Y%m%d)
-	chmod a+x ./.clone
+    cd $WORKDIR
+    GLOBIGNORE=.
+    echo "#! /bin/sh" > ./.clone
+    for subdir in *; do
+        test -d "$subdir" || continue
+        cd "$subdir"
+        remote=$(git remote -v)
+        for word in $remote; do
+            if [ `echo "$word" | grep 'git@' ` ]; then
+                echo "git clone $word" >> ../clone-$(date +%Y%m%d)
+            fi
+        done
+        cd -
+    done
+    awk '!seen[$0]++' ./clone-$(date +%Y%m%d) > ./.clone
+    rm ./clone-$(date +%Y%m%d)
+    chmod a+x ./.clone
 }
 
 clone(){
-	cd $WORKDIR
-	./.clone
-	for d in *; do
-		if [ -f "$d/gitrepos.sh" ]; then
-			cd $d
-			./gitrepos.sh
-			cd $WORKDIR
-		fi
-	done
+    cd $WORKDIR
+    ./.clone
+    for d in *; do
+        if [ -f "$d/gitrepos.sh" ]; then
+            cd $d
+            ./gitrepos.sh
+            cd $WORKDIR
+        fi
+    done
 }
 
 force_update(){
-	cd $WORKDIR
-	for d in *; do
-		if [ -d "$d/.git" ]; then
-			cd $d && git pull
-			cd $WORKDIR
-		fi
-	done
+    cd $WORKDIR
+    for d in *; do
+        if [ -d "$d/.git" ]; then
+            cd $d && git pull
+            cd $WORKDIR
+        fi
+    done
 }
 
 force_sub_update(){
-	cd $WORKDIR
-	for d in *; do
-		if [ -d "$d/.git" ]; then
-			cd $d && git commit -am "$(date)"
-			git push --set-upstream origin master
-			cd $WORKDIR
-		fi
-	done
+    cd $WORKDIR
+    for d in *; do
+        if [ -d "$d/.git" ]; then
+            cd $d && git commit -am "$(date)"
+            git push --set-upstream origin master
+            cd $WORKDIR
+        fi
+    done
 }
 
 #generate a repository.
 genrepo(){ 
-#	This command should be able to generate a repository for upload.
-#	Right now I'm just using apt-git, which is just a little hack I use to
-#	host a few debian packages on github pages. apt-git's probably going to
-#	become pkg-git when it can also structure other types of repositories.
-#	Besides that, it should also be able to do other things, like with aptly
-#	and all the other tools for automatically structuring repositories. This
-#	needs to be done in part to remove dependencies on github itself.
-#	Bitbucket has a similar setup which is cool, but the fewer proprietary
-#	dependencies the better and that means self hosting. Which is easy
-#	because they're just https servers which means you can shoehorn one into
-#	pretty much anything that'll give you the space to do it. Which makes
-#	this all a little daunting, because my whole goal is to make this system
-#	even more approachable, customizable, and decentralized and all those
-#	options leave the definition of those terms open to interpretation.
-	cd $WORKDIR
-        git clone git@github.com:cmotc/fdroid-git.git ../repository
-#	git clone https://www.github.com/cmotc/apt-git/apt-git.git ../repository
-#	git clone https://www.github.com/$HOSTID/apt-git/apt-git.git ../repository
+#    This command should be able to generate a repository for upload.
+#    Right now I'm just using apt-git, which is just a little hack I use to
+#    host a few debian packages on github pages. apt-git's probably going to
+#    become pkg-git when it can also structure other types of repositories.
+#    Besides that, it should also be able to do other things, like with aptly
+#    and all the other tools for automatically structuring repositories. This
+#    needs to be done in part to remove dependencies on github itself.
+#    Bitbucket has a similar setup which is cool, but the fewer proprietary
+#    dependencies the better and that means self hosting. Which is easy
+#    because they're just https servers which means you can shoehorn one into
+#    pretty much anything that'll give you the space to do it. Which makes
+#    this all a little daunting, because my whole goal is to make this system
+#    even more approachable, customizable, and decentralized and all those
+#    options leave the definition of those terms open to interpretation.
+    cd $WORKDIR
+#    git clone git@github.com:cmotc/fdroid-git.git ../repository
+    git clone https://www.github.com/cmotc/apt-git/apt-git.git ../repository
+#    git clone https://www.github.com/$HOSTID/apt-git/apt-git.git ../repository
 }
---
+++ joker_20160111-1.dsc
@@ -0,0 +1,20 @@
+Format: 3.0 (quilt)
+Source: joker
+Binary: joker
+Architecture: any
+Version: 20160111-1
+Maintainer: dyngar <dyngar@dyngar.local>
+Homepage: <insert the upstream URL, if relevant>
+Standards-Version: 3.9.5
+Build-Depends: debhelper (>= 9)
+Package-List:
+ joker deb unknown optional arch=any
+Checksums-Sha1:
+ 96ef3ba8cb2929465fbcd81dfea37a46f63cd7c5 61564 joker_20160111.orig.tar.xz
+ 1854b103da0ecb14c29d9180619d3db920818cd4 2436 joker_20160111-1.debian.tar.xz
+Checksums-Sha256:
+ 839f3564aacc5dfb1f9ee70c1c2ded2ffe6eaf0de62bccefe3bf22163b12c48e 61564 joker_20160111.orig.tar.xz
+ 7abb6a67873e6972445b33bb58400514bc0415ae727df4b9fa859f4528a1c6de 2436 joker_20160111-1.debian.tar.xz
+Files:
+ 1fde763b559f869643baa16ef86be225 61564 joker_20160111.orig.tar.xz
+ ef7246c3e62a2f14b928fa2f670c88aa 2436 joker_20160111-1.debian.tar.xz
