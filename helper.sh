3ccf3aa1b7d145961bd61c137c1924731b4c0645
idk <idk@idk.local>
2016-03-27 01:19:42 -0400

    finally got the preferred sources set up

--- helper.sh
+++ helper.sh
@@ -258,9 +258,8 @@ clone(){
 				else
 					httpsline=$(echo "$tmpline" | sed 's|git@github.com:|https://github.com/|')
 					sshline=$(echo "$tmpline" | sed 's|https://github.com/|git@github.com:|')
-					echo "test $sshline"
 					git clone $sshline #1> /dev/null
-					if [ -d "$t/.git" ]; then
+					if [ ! -d "$t/.git" ]; then
 						git clone $httpsline
 					fi
 				fi
